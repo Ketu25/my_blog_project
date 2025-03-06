4c7bd823394d8232cfff3a28462ba68ca3636b42
Ketu Patel <58966891+Ketu25@users.noreply.github.com>
2025-03-05 22:16:24 -0500

    Initial Commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+
 from .models import Post
 from .forms import CommentForm
 
@@ -6,6 +11,7 @@ def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all().order_by('created_date')
@@ -25,4 +31,13 @@ def post_detail(request, pk):
         'form': form
     })
 
-
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after signup
+            return redirect('post_list')  # Redirect to your blog homepage
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- myblog/settings.py
+++ myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,6 +111,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+LOGIN_REDIRECT_URL = '/'      # Redirect to homepage after login
+LOGOUT_REDIRECT_URL = '/'     # Redirect to homepage after logout
 
 
 # Static files (CSS, JavaScript, Images)
