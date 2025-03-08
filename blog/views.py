2d891852baeed97f24040517e007db4ab8a800d0
Ketu Patel <58966891+Ketu25@users.noreply.github.com>
2025-03-08 12:13:57 -0500

    Publish

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
+from django.http import HttpResponseForbidden
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-
-
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
+from .forms import PostForm
+from .models import Post
 
 def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
@@ -41,3 +44,26 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+@login_required
+def post_create(request):
+    if not request.user.is_staff:
+        return HttpResponseForbidden("You do not have permission to add posts.")
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # Optionally, set additional fields here (e.g., post.author = request.user)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@staff_member_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
